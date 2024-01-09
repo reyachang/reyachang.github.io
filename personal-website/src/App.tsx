40b2e2ce73a40a9f3150ed569062185498080119
Chang <changling.huang@live.com>
2024-01-08 20:58:20 -0800

    Create menu for personal website

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
+=======
+import { Image, Layout, Menu, MenuProps } from 'antd';
+import { HomeOutlined, HeartOutlined, LaptopOutlined } from '@ant-design/icons';
+
+import profilePicture from './img/chang.png';
+
+import './App.css';
+import Sider from 'antd/es/layout/Sider';
+
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    label: 'home',
+    icon: <HomeOutlined />
+  },
+  {
+    key: '2',
+    label: 'projects',
+    icon: <HeartOutlined />
+  },
+  {
+    key: '3',
+    label: 'blog',
+    icon: <LaptopOutlined />
+  },
+];
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +48,26 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+          <Layout>
+            <Sider>
+      <Image
+      src={profilePicture}
+      alt="A picture of Chang holding icecream."
+      width={100}
+      height={100}
+      style={{borderRadius: '50%', padding: "15px" }}
+      />
+      <Menu
+          theme="dark"
+          mode="vertical"
+          defaultSelectedKeys={['1']}
+          items={items}
+          style={{ flex: 1, minWidth: 0, height:"100vh"}}
+        />
+       </Sider>
+       </Layout>
+>>>>>>> Stashed changes
     </div>
   );
 }
